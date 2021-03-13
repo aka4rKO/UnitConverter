06c19af24c74e26c9d5104706e5ec7d97e5d7c82
aka4rKO <akram.arm.7@gmial.com>
2021-03-13 14:29:06 -0800

    history page table view

--- UnitConverter/controllers/HistoryViewController.swift
+++ UnitConverter/controllers/HistoryViewController.swift
@@ -7,23 +7,62 @@
 
 import UIKit
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    let cars = ["Tesla", "Nissan", "Lambo", "Jeep"]
+    let fruits = ["Banana", "Papaya", "Grapes"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            return cars.count
+        case 1:
+            return fruits.count
+        case 2:
+            return cars.count
+        case 3:
+            return fruits.count
+        case 4:
+            return 0
+        default:
+            break
+        }
+        
+        return 0
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            cell.textLabel?.text = cars[indexPath.row]
+        case 1:
+            cell.textLabel?.text = fruits[indexPath.row]
+        case 2:
+            cell.textLabel?.text = cars[indexPath.row]
+        case 3:
+            cell.textLabel?.text = fruits[indexPath.row]
+        case 4:
+            return cell
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        tableView.reloadData()
+    }
 
 }
